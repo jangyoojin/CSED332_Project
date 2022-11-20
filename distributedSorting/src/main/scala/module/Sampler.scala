1ee09c462c756d286eee73dbec701190126d96a4
sangy00n <dbs1367@postech.ac.kr>
2022-11-20 20:47:35 +0900

    Add: Sampler module in Sampler.scala

---
+++ distributedSorting/src/main/scala/module/Sampler.scala
@@ -0,0 +1,15 @@
+package module
+
+import java.io.{File, FileOutputStream}
+import scala.io.Source
+
+object Sampler {
+  def sample(inputDirPath: String, sampleDirPath: String, sampleSize: Int): Unit = {
+    val inputDir = new File(inputDirPath)
+    val headFile = inputDir.listFiles.filter(_.isFile).toList.head
+    val source = Source.fromFile(headFile)
+    val sampleFileName = sampleDirPath + "/sample"
+    val writer = new FileOutputStream(new File(sampleFileName))
+    writer.write("contents to write".getBytes)
+  }
+}
