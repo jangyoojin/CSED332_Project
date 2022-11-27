b3bce19176f6b78c826cee01af81e3b10ce0b111
dmsgk724 <dmsgk724@postech.ac.kr>
2022-11-27 17:13:25 +0900

    Add: FileClient/FileServer

--- distributedSorting/src/main/scala/Utils/FileIO.scala
+++ distributedSorting/src/main/scala/Utils/FileIO.scala
@@ -1,7 +1,37 @@
 package Utils
 
-class FileIO
+
+import java.io.File
+
+
+object FileIO
 {
+  def getFile(path : String, prefix : String):List[File] =
+    {
+      val dir = new File(path);
+      val Files=dir.listFiles();
+      if(dir.isDirectory&&dir.exists())
+        {
+          val temps =Files.filter(x=> x.isFile)
+          if (prefix==null) {
+            temps.toList
+          }
+          else temps.filter( x=>x.getName.startsWith(prefix)).toList
+        }
+      else
+        {
+          List[File]()
+        }
+
+    }
+
+  def createFile(path:String, prefix: String): File={
+    val filename=path+prefix
+    var dir = new File(filename)
+    dir.createNewFile()
+    dir
+  }
+
 
 
 }
