55815b26e1ff444b6765521315e2ef02658df029
장유진(컴퓨터공학과) <jangyj2020@postech.ac.kr>
2022-12-02 09:55:19 +0900

    FIX: add synchronize in start function when we access to workers list

--- distributedSorting/src/main/scala/network/NetworkServer.scala
+++ distributedSorting/src/main/scala/network/NetworkServer.scala
@@ -1,16 +1,15 @@
 package network
 
+import io.grpc.stub.StreamObserver
+
 import scala.concurrent.{Await, ExecutionContext, Future, Promise}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.collection.mutable.Map
-
 import java.util.concurrent.TimeUnit
 import java.util.logging.Logger
 import java.io.{File, FileOutputStream, OutputStream}
 import java.net._
-
 import io.grpc.{Server, ServerBuilder, Status}
-
 import message.connection._
 import utils._
 
@@ -64,19 +63,62 @@ class NetworkServer(executionContext: ExecutionContext, port:Int, workerNum: Int
       if (state != MINIT) Future.failed(new Exception("fail to connect"))
       else {
         logger.info(s"Start: Worker ${request.ip}:${request.port} send StartRequest")
-        if(workers.size < workerNum) {
-          workers(workers.size + 1) = new WorkerData(workers.size + 1, request.ip, request.port)
-          if(workers.size == workerNum) {
-            state = MSTART
+        workers.synchronized {
+          if (workers.size < workerNum) {
+            workers(workers.size + 1) = new WorkerData(workers.size + 1, request.ip, request.port)
+            if (workers.size == workerNum) {
+              state = MSTART
+            }
+            Future.successful(new StartResponse(workers.size))
+          } else {
+            Future.failed(new Exception("[start] there are too many workers"))
           }
-          Future.successful(new StartResponse(workers.size))
-        } else {
-          Future.failed(new Exception("[start] there are too many workers"))
         }
       }
     }
 
-    override def sample(request: SampleRequest): Future[SampleResponse] =
+    override def sample(request: StreamObserver[SampleResponse]): StreamObserver[SampleRequest] = {
+      if (state != MINIT && state != MSTART) {
+        new StreamObserver[SampleRequest] {
+          override def onNext(value: SampleRequest): Unit = {
+            new Exception("[sample] Wrong server state")
+          }
+          override def onError(t: Throwable): Unit = {}
+          override def onCompleted(): Unit = {}
+        }
+      } else {
+        logger.info("[sample] Worker send sample")
+        new StreamObserver[SampleRequest] {
+          var id = -1
+          var fileNum = 0
+          var writer = null
+          override def onNext(value: SampleRequest): Unit = {
+            id = value.workerId
+            fileNum = value.inputFileNum
+            //1. make a file to write the sample data in serverDir(: ./master/)
+            if (writer == null) {
+              writer = new FileOutputStream(FileIO.createFile(serverDir, s"sample_${value.workerId}"))
+            }
+            //2. write sample data of value(the request) to sample_00
+            value.data.writeTo(writer)
+            writer.flush
+          }
+
+          override def onError(t: Throwable): Unit = {
+            logger.warning(s"[sample] Worker $id fail to send a sample data ${Status.fromThrowable(t)}")
+            throw t
+          }
+
+          override def onCompleted(): Unit = {
+            logger.info(s"[sample] Worker $id done")
+            writer.close
+            request.onNext(new SampleResponse(status = Stat.SUCCESS))
+            request.onCompleted()
+
+          }
+        }
+      }
+    }
 
     override def divide(request: DivideRequest): Future[DivideResponse] = ???
 
