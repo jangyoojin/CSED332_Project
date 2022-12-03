2961cc0f9ad475e8cfe14b162d2af1d4443481f4
dmsgk724 <dmsgk724@postech.ac.kr>
2022-12-03 14:47:32 +0900

    FIX :input folder change

--- distributedSorting/src/main/scala/module/Sampler.scala
+++ distributedSorting/src/main/scala/module/Sampler.scala
@@ -1,13 +1,12 @@
 package module
 
-import java.io.{File, FileOutputStream}
 import scala.io.Source
+import java.io.{File,FileOutputStream}
 
 object Sampler {
   def sample(inputDirPath: String, sampleDirPath: String, sampleSize: Int): Unit = {
     val inputDir = new File(inputDirPath)
     val headFile = inputDir.listFiles.filter(_.isFile).toList.head
-
     val source = Source.fromFile(headFile)
     val sampleFileName = sampleDirPath + "/sample"
     val writer = new FileOutputStream(new File(sampleFileName))
