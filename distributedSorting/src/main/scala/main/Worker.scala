c2eaed5795ed952137768fa714598cbef70c7304
sangy00n <dbs1367@postech.ac.kr>
2022-12-06 20:42:54 +0900

    Fix: Worker's main() in Worker.scala

--- distributedSorting/src/main/scala/main/Worker.scala
+++ distributedSorting/src/main/scala/main/Worker.scala
@@ -8,26 +8,23 @@ object Worker {
   def main(args: Array[String]): Unit = {
     val clientData = Parser.parse(args)
     val networkClient = new NetworkClient(clientData)
-    try {
-      networkClient.requestStart
-      networkClient.sample
 
-      val samplePromise = Promise[Unit]()
-      networkClient.requestSample(samplePromise)
-      Await.ready(samplePromise.future, duration.Duration.Inf)
+    networkClient.requestStart
+    networkClient.sample
 
-      networkClient.requestDivide
-      networkClient.partition
-      networkClient.startFileServer
-      networkClient.requestShuffle
-      networkClient.shuffle
-      networkClient.requestSort
-      networkClient.stopFileServer
-      networkClient.subPartition
-      networkClient.sort
-      networkClient.shutdown
-    } finally {
-      networkClient.shutdown
-    }
+    val samplePromise = Promise[Unit]()
+    networkClient.requestSample(samplePromise)
+    Await.ready(samplePromise.future, duration.Duration.Inf)
+
+    networkClient.requestDivide
+    networkClient.partition
+    networkClient.startFileServer
+    networkClient.requestShuffle
+    networkClient.shuffle
+    networkClient.requestSort
+    networkClient.stopFileServer
+    networkClient.subPartition
+    networkClient.sort
+    networkClient.shutdown
   }
 }
