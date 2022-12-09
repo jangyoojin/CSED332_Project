7aca6bb2d8b576894cd4098173b4c10d14de490c
장유진(컴퓨터공학과) <jangyj2020@postech.ac.kr>
2022-12-10 00:24:02 +0900

    Fix: fix SortingTest to check order between files

--- distributedSorting/src/test/scala/SortingTest.scala
+++ distributedSorting/src/test/scala/SortingTest.scala
@@ -4,27 +4,43 @@ import java.io.File
 import java.nio.file.{Files, Paths}
 import scala.io.Source
 
+
 class SortingTest extends AnyFunSuite {
   test("sort") {
-    var bytes = 0
     var i = 0
-    val path = "/home/pink/64/output/"
     val fileList = FileIO.getFile("/home/pink/64/output", null)
-    for (file <- fileList) {
-      bytes += Files.size(Paths.get(path + file.getName)).toInt
-      assert(sort(file) == true)
-      i = i + 1
-    }
-    println("Bytes of the sum of output files are" + bytes)
+//    for (file <- fileList) {
+//      bytes += Files.size(Paths.get(path + file.getName)).toInt
+//      assert(sort(file) == true)
+//      i = i + 1
+//    }
+    assert(sort(fileList))
   }
 
-  def sort(file: File): Boolean = {
+//  def sort(file: File): Boolean = {
+//    var result = true
+//    val prev = " " * 10
+//    val srcLines = Source.fromFile(file).getLines()
+//    for (line <- srcLines) {
+//      if (line.substring(0, 10) < prev) result = false
+//    }
+//    result
+//  }
+
+  def sort(fileList: List[File]): Boolean = {
+    var bytes = 0
+    val path = "/home/pink/64/output/"
     var result = true
-    val prev = " " * 10
-    val srcLines = Source.fromFile(file).getLines()
-    for (line <- srcLines) {
-      if (line.substring(0, 10) < prev) result = false
+    var prev = " " * 10
+    for (file <- fileList) {
+      bytes += Files.size(Paths.get(path + file.getName)).toInt
+      val srcLines = Source.fromFile(file).getLines()
+      for (line <- srcLines) {
+        if (line.substring(0, 10) < prev) result = false
+        prev = line.substring(0, 10)
+      }
     }
+    println("Bytes of the sum of output files are " + bytes)
     result
   }
 
